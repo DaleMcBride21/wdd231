7c021ea1e16e6e051ca001ac6955bd4ed7c8f506
DaleMcBride21 <dlmcburrito@gmail.com>
2024-10-22 10:57:17 -0600

    update symbols

---
+++ src/localStorage/notekeeper.js
@@ -0,0 +1,19 @@
+window.addEventListener("loaded", () => {
+    let savedNote = localStorage.getItem('note');
+    if (savedNote) {
+        document.querySelector('#noteInput').value = savedNote;
+    }
+})
+
+document.querySelector('#saveBtn').addEventListener('click', function() {
+    let note = document.querySelector('#noteInput').value;
+    localStorage.setItem('note', note);
+    alert('Note saved');
+})
+
+document.querySelector('#clearBtn').addEventListener('click', function() {
+    document.querySelector('#noteInput').value = '';
+    localStorage.removeItem('note');
+    alert('Note removed');
+})
+
